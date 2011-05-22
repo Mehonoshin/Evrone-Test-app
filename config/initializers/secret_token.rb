bb0edca09cf4550b2b27e582fe4f275c1d70b63d
Stanislav Mekhonoshin <mehonoshin@sc.vsu.ru>
2011-05-22 23:27:43 +0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EvroneBlog::Application.config.secret_token = 'c1655fa84bf4387de14d7278a17bf32c8f66fa022f9a07202a41296f5f1af7d3d1a9ab3a6ccf7b5ad31f1655092d1e64acba2f54d8a185a21f5cff2e2afb3f7a'
