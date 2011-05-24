b5885e554bb2936320d0dbf85ed4c687d9ce6905
Stanislav Mekhonoshin <mehonoshin@sc.vsu.ru>
2011-05-24 12:02:27 +0400

    Pagination added

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,18 +1,23 @@
 class PostsController < ApplicationController
   
   def index
-    @postsList = Post.find(:all, :order => "created_at DESC")
+    @postsList = Post.paginate :page => params[:page], :order => 'created_at DESC'
   end
   
   def create
     if user_signed_in?
       p = Post.create(:title => params[:title], :textbody => params[:textbody])
+      redirect_to post_path(p)
+    else
+      redirect_to root_path
     end
-    redirect_to post_path(p)
+    
   end
   
   def new
-    # show form
+    if not user_signed_in?
+      redirect_to root_path
+    end
   end
   
   def show
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,5 @@
 class Post < ActiveRecord::Base
   belongs_to :user
+  cattr_reader :per_page
+  @@per_page = 5
 end
