8eb32a13605f5c232a852873bcf7b0401a4f274d
Stanislav Mekhonoshin <mehonoshin@sc.vsu.ru>
2011-05-25 14:46:19 +0400

    Tags are editable, viewable

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -7,7 +7,7 @@ class PostsController < ApplicationController
   
   def create
     if user_signed_in?
-      p = Post.create(:title => params[:post][:title], :textbody => params[:post][:textbody], :user_id => current_user.id)
+      p = Post.create(:title => params[:post][:title], :textbody => params[:post][:textbody], :user_id => current_user.id, :tag_list => params[:post][:tag_list])
       redirect_to post_path(p)
     else
       redirect_to root_path
@@ -31,6 +31,7 @@ class PostsController < ApplicationController
     if (current_user.id == post.user_id)    
       post.title = params[:post][:title]
       post.textbody = params[:post][:textbody]
+      post.tag_list = params[:post][:tag_list]
       post.save
     end
     redirect_to post_path(post)
